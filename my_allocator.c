e852deaaabb1934f2cf99fc595bbf3abcc489999
AZMomin <ali.shan1014@gmail.com>
2014-07-14 02:10:59 -0500

    BUGGY
    delete headers from memory, not just remove from list

--- my_allocator.c
+++ my_allocator.c
@@ -31,15 +31,14 @@
 /*--------------------------------------------------------------------------*/
 
     typedef struct header{
-       int MagicNumber;			//initial check to ensure correct address
-       int  is_free;            // 0 = free, 1 = not free
-       int binary_Index;        // 2^binary_index = size
-       int side;                // 0 = left; 1 = right; NULL = none
-       int Inheritance;			// 0 = left inheritane, 1 = right, NULL = none
-       int size;                // size of the block 
-       struct header* next;		// points to next block
-       struct header* prev;		// points to previous block
-} ;
+      int MagicNumber;			//initial check to ensure correct address
+      int  is_free;            // 0 = free, 1 = not free
+      int binary_Index;        // 2^binary_index = size
+      int side;                // left = 0; right = 1; no side = 2
+      int size;                // size of the block 
+      struct header* next;		// points to next block
+      struct header* prev;		// points to previous block
+} HDR;
 
 /*--------------------------------------------------------------------------*/
 /* CONSTANTS */
@@ -47,7 +46,7 @@
 
   int BlockNum = 0;
   int index = 0;
-  void** freeList;
+  HDR** freeList;
   int totalLength;
   void * totalMemory;
   int blocksize;
@@ -63,232 +62,152 @@
 /*--------------------------------------------------------------------------*/
 
 
-/*void MergeBlocks ( HDR * _h){
-  if( _h -> MagicNumber == 3028)         // Check magic number
-    if (_h -> is_free == 0)              // if it's free
+//*******************************************************
+//            REMOVE FROM FREELIST
+//*******************************************************
+void remove(HDR * _h){
+  if(_h -> prev == NULL && _h -> next == NULL){          // only element in list
+           printf("%s\n", "Remove: Only element in List");
+          freeList[_h ->binary_Index] = NULL;
+        }
+  if(_h -> prev == NULL && _h -> next != NULL){         // first element in list
+          printf("%s\n", "Remove: First in List");
+          freeList[_h ->binary_Index] = _h -> next;
+          _h -> next -> prev = NULL;
+          _h -> next = NULL;
 
-      if (_h -> side == 0)               // if it's a left block
-        if ((_h+_h->size)->MagicNumber == 3028)
-          if ((_h+_h->size)->is_free == 0){       // if it's buddy is free
-            HDR * _h1 = (HDR *)(_h+_h->size);
-            _h -> size = _h->size + (_h1)->size;   // add sizes of buddies
-            // add a way to check if top block is reached. If yes then set
-            // set side = 2; Inheritance = 2; 
-            _h -> side = _h -> Inheritance; 
-            _h -> Inheritance = (_h1)-> Inheritance;
-            _h -> fibIndex = _h -> fibIndex + 1;
-            
-
-  // remove _h from free list 
-
-            if (_h -> prev == NULL && _h -> next == NULL){    // first of list
-                freeList[_h -> fibIndex -1] = NULL;
-               // freeList[_h -> fibIndex] = NULL;
-            }
-            else if (_h -> prev == NULL && _h -> next != NULL){
-                freeList[_h -> fibIndex -1] = _h -> next;
-                _h -> next -> prev = NULL;
-                _h -> next = NULL;
-            }
-            else if(_h -> prev != NULL && _h -> next == NULL){
-              _h -> prev -> next == NULL;
-              _h -> prev = NULL;
-            }
-            else if (_h -> prev != NULL && _h -> next != NULL){
-              _h -> prev -> next = _h -> next;
-              _h -> next -> prev = _h -> prev;
-              _h -> prev = NULL;
-              _h -> next = NULL;
-            }
-            else {}
-
-  // remove (_h1) from free list
-
-            if ((_h1) -> prev == NULL && (_h1) -> next == NULL){    // first of list
-                freeList[(_h1) -> fibIndex] = NULL;
-                //freeList[(_h1) -> fibIndex] = NULL;
-            }
-            else if ((_h1) -> prev == NULL && (_h1) -> next != NULL){
-                freeList[(_h1) -> fibIndex] = (_h1) -> next;
-                (_h1) -> next -> prev = NULL;
-                (_h1) -> next = NULL;
-            }
-            else if((_h1) -> prev != NULL && (_h1) -> next == NULL){
-              (_h1) -> prev -> next == NULL;
-              (_h1) -> prev = NULL;
-            }
-            else if ((_h1) -> prev != NULL && (_h1) -> next != NULL){
-              (_h1) -> prev -> next = (_h1) -> next;
-              (_h1) -> next -> prev = (_h1) -> prev;
-              (_h1) -> prev = NULL;
-              (_h1) -> next = NULL;
-            }
-            else {}
-
-  // add _h to proper free list 
-
-            if (freeList[_h -> fibIndex] == NULL)
-              freeList[_h -> fibIndex] = _h;
-            else{
-              freeList[_h -> fibIndex] -> prev = _h;           
-              _h -> next = freeList[_h -> fibIndex];
-              freeList[_h -> fibIndex] = _h; 
-            }
-
-          MergeBlocks(_h);     
-          }
-
-
-     else if (_h -> side == 1)
-      {
-        int i = (_h-> size / fib(_h -> fibIndex));    // represents block size 
-        i = i* fib((_h->fibIndex)+1);                 // represents size of left block
-        HDR * _h1 = (HDR *)(_h - i); 
-
-        if ((_h1)->MagicNumber == 3028)
-          if ((_h1)->is_free == 0){       // if it's buddy is free
-            _h1 ->size = _h->size + (_h1)->size;   // add sizes of buddies
-            _h1 -> side = _h1 -> Inheritance; 
-            _h1 -> Inheritance = _h-> Inheritance;
-            _h1 ->  fibIndex = _h -> fibIndex + 2;
-            
-      
-  // remove _h from free list 
-
-            if (_h -> prev == NULL && _h -> next == NULL){    // first of list
-                freeList[_h -> fibIndex] = NULL;
-               // freeList[_h -> fibIndex] = NULL;
-            }
-            else if (_h -> prev == NULL && _h -> next != NULL){
-                freeList[_h -> fibIndex] = _h -> next;
-                _h -> next -> prev = NULL;
-                _h -> next = NULL;
-            }
-            else if(_h -> prev != NULL && _h -> next == NULL){
-              _h -> prev -> next == NULL;
-              _h -> prev = NULL;
-            }
-            else if (_h -> prev != NULL && _h -> next != NULL){
-              _h -> prev -> next = _h -> next;
-              _h -> next -> prev = _h -> prev;
-              _h -> prev = NULL;
-              _h -> next = NULL;
-            }
-            else {}
-
-  // remove (_h1) from free list
-
-            if ((_h1) -> prev == NULL && (_h1) -> next == NULL){    // first of list
-                freeList[(_h1) -> fibIndex-1] = NULL;
-                //freeList[(_h1) -> fibIndex] = NULL;
-            }
-            else if ((_h1) -> prev == NULL && (_h1) -> next != NULL){
-                freeList[(_h1) -> fibIndex-1] = (_h1) -> next;
-                (_h1) -> next -> prev = NULL;
-                (_h1) -> next = NULL;
-            }
-            else if((_h1) -> prev != NULL && (_h1) -> next == NULL){
-              (_h1) -> prev -> next == NULL;
-              (_h1) -> prev = NULL;
-            }
-            else if ((_h1) -> prev != NULL && (_h1) -> next != NULL){
-              (_h1) -> prev -> next = (_h1) -> next;
-              (_h1) -> next -> prev = (_h1) -> prev;
-              (_h1) -> prev = NULL;
-              (_h1) -> next = NULL;
-            }
-            else {}
-
-  // add _h1 to proper free list 
-
-            if (freeList[_h1 -> fibIndex] == NULL)
-              freeList[_h1 -> fibIndex] = _h1;
-            else{
-              freeList[_h1 -> fibIndex] -> prev = _h1;           
-              _h1 -> next = freeList[_h1 -> fibIndex];
-              freeList[_h1 -> fibIndex] = _h1; 
-            }
-
-          MergeBlocks(_h1);     
-          }
-      }
-     else {}
-}
-
-void SplitBlocks ( HDR * _h){
-  HDR * _hR = _h + fib(_h -> fibIndex -1) * blocksize; 
-
-       if( _h -> MagicNumber == 3028)         // Check magic number
-        if (_h -> is_free == 0){            // Check if free
-    // set right block values 
-          _hR -> size = blocksize * fib(_h fibIndex -2);
-          _hR -> fibIndex = _h -> fibIndex -2;
-          _hR -> Inheritance = _h -> Inheritance;
-          _hR -> side = 1;
-    // set left block
-          _h -> fibIndex = _h -> fibIndex -1;
-          _h -> size = fib(_h -> fibIndex-1)*blocksize;
-          _h -> Inheritance = _h -> side;
-          _h -> side = 0;
-          //change _h free list location
-         if (_h -> prev == NULL && _h -> next == NULL){    // first of list
-                freeList[_h -> fibIndex +1] = NULL;
-            }
-            else if(_h -> prev != NULL && _h -> next == NULL){
-              _h -> prev -> next == NULL;
-              _h -> prev = NULL;
-            }
-            else if (_h -> prev == NULL && _h -> next != NULL){
-                freeList[_h -> fibIndex +1] = _h -> next;
-                _h -> next -> prev = NULL;
-                _h -> next = NULL;
-            }
-            else if (_h -> prev != NULL && _h -> next != NULL){
-              _h -> prev -> next = _h -> next;
-              _h -> next -> prev = _h -> prev;
-              _h -> prev = NULL;
-              _h -> next = NULL;
-            }
-            else {}
-          // add to new free list 
-              if (freeList[_h -> fibIndex] == NULL)
-              freeList[_h -> fibIndex] = _h;
-            else{
-              freeList[_h -> fibIndex] -> prev = _h;           
-              _h -> next = freeList[_h -> fibIndex];
-              freeList[_h -> fibIndex] = _h; 
-            }
-    // free up left block from memory 
-            if (_hR -> prev == NULL && _hR -> next == NULL){    // first of list
-                freeList[_hR -> fibIndex +1] = NULL;
-            }
-            else if(_hR -> prev != NULL && _hR -> next == NULL){
-              _hR -> prev -> next == NULL;
-              _hR -> prev = NULL;
-            }
-            else if (_hR -> prev == NULL && _hR -> next != NULL){
-                freeList[_hR -> fibIndex +1] = _hR -> next;
-                _hR -> next -> prev = NULL;
-                _hR -> next = NULL;
-            }
-            else if (_hR -> prev != NULL && _hR -> next != NULL){
-              _hR -> prev -> next = _hR -> next;
-              _hR -> next -> prev = _hR -> prev;
-              _hR -> prev = NULL;
-              _hR -> next = NULL;
-            }
-            else {}
-  // add to new free list 
-              if (freeList[_hR -> fibIndex] == NULL)
-              freeList[_hR -> fibIndex] = _hR;
-            else{
-              freeList[_hR -> fibIndex] -> prev = _hR;           
-              _hR -> next = freeList[_hR -> fibIndex];
-              freeList[_hR -> fibIndex] = _h; 
-            }
+        }
+        if(_h -> prev != NULL && _h -> next == NULL){         // last element in list
+           printf("%s\n", "Remove: Last in List");
+          _h -> prev -> next = NULL;
+          _h -> prev = NULL;
+        }
+        if(_h -> prev != NULL && _h -> next != NULL){         // in between list
+           printf("%s\n", "Remove: Middle in List");
+          _h -> prev -> next = _h -> next;
+          _h -> next -> prev = _h -> prev;
+          _h -> prev = NULL;
+          _h -> next = NULL;
         }
 }
-*/
+//*******************************************************
+//           SPLIT BLOCK
+//*******************************************************
+void SplitBlocks ( HDR * _h){
+ 
+    if(_h -> MagicNumber == 3028){
+        int b_index = _h -> binary_Index -1;
+        int newsize = pow(2,b_index);
+        HDR * _hR = (Addr)(_h + newsize);     // error need to change _hR's adress 
+        // remove _h from freelist
+        remove(_h);
+        // set variables for _h and _hR
+        _h -> MagicNumber = 3028;      
+        _h ->  is_free = 0;            
+        _h -> binary_Index = b_index;        
+        _h -> side = 0;                
+        _h -> size = newsize;                 
+        _h -> next = _hR;    
+        
+        _hR -> MagicNumber = 3028;      
+        _hR ->  is_free = 0;            
+        _hR -> binary_Index = b_index;        
+        _hR -> side = 1;                
+        _hR -> size = newsize;                 
+        _hR -> prev = _h;
+        // assign headers to freeList
+        if(freeList[b_index] == NULL){  
+          freeList[b_index] = _h;
+          _h -> prev = NULL;
+          _hR -> next = NULL;
+        }
+        else{
+          _hR -> next = freeList[b_index];
+          freeList[b_index] -> prev = _hR;
+          _h -> prev = NULL;
+          freeList[b_index] = _h;
+        }
+        printf("%s %d %d\n"," location of _h with size of:", _h, newsize);
+        printf("%s %d %d\n"," location of _hR with size of:", _hR , newsize);
+    }
+    else
+      printf("%s\n", "Error no magic number found, ");
+    
+}
+//*******************************************************
+//             MERGE BLOCKS
+//*******************************************************
+void MergeBlocks ( HDR * _h){
+  int newindex = _h -> binary_Index +1;
+  int newsize = pow(2,newindex);
+  HDR * _h_ = NULL;
+
+  if(_h->MagicNumber == 3028){
+      if (_h -> side == 0){                      // if right block side check if right block is free
+         HDR * temp = (Addr)(_h + _h->size);
+         _h_ = temp;}
+      else{                                      // vice-versa
+         HDR * temp = (Addr)(_h - _h->size);
+         _h_ = temp;}
+
+      if(_h -> is_free == 0){                   
+        if(_h_ -> MagicNumber == 3028){
+            if(_h_ -> is_free == 0){             // remove 2 blocks from free list
+               remove(_h);
+               remove(_h_);
+               if (_h -> side == 0){            // assign new values to the block on left (must check which block is left )
+                  _h -> MagicNumber = 3028;      
+                  _h ->  is_free = 0;            
+                  _h -> binary_Index = newindex;        
+                  _h -> side = 2;                
+                  _h -> size = newsize; 
+                  if(freeList[newindex] == NULL){   // add the updated header into freeList
+                    freeList[newindex] = _h;
+                    _h -> prev = NULL;
+                    _h -> next = NULL;
+                  }
+                  else{
+                    _h -> next = freeList[newindex];
+                    freeList[newindex] -> prev = _h;
+                    _h -> prev = NULL;
+                    freeList[newindex] = _h;
+                  }                
+                   
+               }
+               else{
+                  _h_ -> MagicNumber = 3028;      
+                  _h_ ->  is_free = 0;            
+                  _h_ -> binary_Index = newindex;        
+                  _h_ -> side = 2;                
+                  _h_ -> size = newsize; 
+                  if(freeList[newindex] == NULL){  
+                    freeList[newindex] = _h_;
+                    _h_ -> prev = NULL;
+                    _h_ -> next = NULL;
+                  }
+                  else{
+                    _h_ -> next = freeList[newindex];
+                    freeList[newindex] -> prev = _h_;
+                    _h_ -> prev = NULL;
+                    freeList[newindex] = _h_;
+                  }
+               }
+
+            }
+            else
+              printf("%s\n","Memory Location for second block is not free" );
+
+        }
+        else
+          printf("%s\n","Memory Location for second block is not valid" );
+      }    
+      else
+        printf("%s\n","Memory Location for first block is not free" );
+      }
+  else
+    printf("%s\n","Memory Location for initial block is not valid" );
+}
+
 int release_allocator(){
   free(freeList);
   free(totalMemory);
@@ -307,23 +226,22 @@ extern unsigned int init_allocator(unsigned int _basic_block_size, unsigned int 
     }
   }
 // creates free list of "index" spaces 
-  *freeList = malloc(index * sizeof(Addr));
 // allocate total amount of space
   totalMemory = malloc(BlockNum * _basic_block_size);
+  freeList = (HDR**)malloc(index * sizeof(HDR));
 // create HDR for total memory 
-  header * a = (header*)totalMemory;
+  HDR * a = (HDR*)totalMemory;
   a-> MagicNumber = 3028;
   a-> is_free = 0;				
-  a-> binary_Index = index;		
-  a-> side = 2;					
-  a-> Inheritance = 2;			
+  a-> binary_Index = index;
+  a-> side = 2;				
   a-> size = BlockNum * _basic_block_size;
   a-> next = NULL;
   a-> prev = NULL;
-
   freeList[index] = a;  
   
   totalLength = BlockNum * _basic_block_size;
+  totalMemory = totalMemory + sizeof(HDR);
   return totalLength;
 
 }
@@ -332,7 +250,40 @@ extern unsigned int init_allocator(unsigned int _basic_block_size, unsigned int 
 
 /* Don't forget to implement "init_allocator" and "release_allocator"! */
 
-
+extern void check_list(){//int list_index) {
+    if(freeList[index] != NULL){
+      printf("%s %d %d\n", "Size of Free List", index, freeList);
+      printf("%s %d\n", "Pointer of header of first element" ,freeList[index] );
+      printf("%s %d\n", "Pointer to toal memory", totalMemory );
+      printf("%s\n\n","Starting Split 1");
+      SplitBlocks(freeList[index]);
+      printf("%s\n\n","Starting Split 2");
+      SplitBlocks(freeList[index-1]);
+      printf("%s\n\n","Starting Split 3");
+      SplitBlocks(freeList[index-2]);
+      printf("%s\n\n","Starting Split 4");
+      SplitBlocks(freeList[index-3]);
+      printf("%s\n\n","Starting Split 5");
+      SplitBlocks(freeList[index-4]);
+      print_freeList();
+      printf("%s\n\n","Starting Merge");
+      MergeBlocks(freeList[index-1]);
+      // BUGGY BUGGY BUGGY
+     // printf("%s\n\n","Starting Merge 2");
+     // MergeBlocks(freeList[index-1]);
+      //printf("%d\n", (HDR*) freeList[index]->MagicNumber );
+    }
+}
+extern void print_freeList(){
+  int i;
+  for (i = 0 ; i <= index; i++){
+    int a = pow(2, i);
+    if(freeList[i] == NULL)
+      printf("%s %d\n", "No elements of size: ", a);
+    else
+      printf("%s %d\n", "Element found at size", a);
+  }
+}
 extern Addr my_malloc(unsigned int _length) {
   /* This preliminary implementation simply hands the call over the 
      the C standard library! 
