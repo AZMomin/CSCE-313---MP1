e916ed7a6542a754417fd9b26e007934bfb19206
AZMomin <ali.shan1014@gmail.com>
2014-07-14 02:18:00 -0500

    Update memtest.c

--- memtest.c
+++ memtest.c
@@ -1,10 +1,12 @@
-#include <iostream>
+#include <stdio.h>
 #include "my_allocator.h"
 
-using namespace std;
 int main(int argc, char ** argv) {
-	int a = init_allocator(1, 15);
-	cout << a << endl;
-
+	int a = init_allocator(4, 10000);
+	printf("%s %d\n", "Total memory allocated" ,a );
+	printf("%s %d\n", "Total blocks" ,a/4 );
+	print_freeList();
+	check_list();
+	print_freeList();
 	return 0;
 }
