4ef9ebc05e5fac109a02ce0ccfccec7af50bd31c
AZMomin <ali.shan1014@gmail.com>
2014-07-15 17:02:25 -0500

    Update my_allocator.c

--- my_allocator.c
+++ my_allocator.c
@@ -97,43 +97,47 @@ void _remove(HDR * _h){
 //*******************************************************
 void SplitBlocks ( HDR * _h){
     if(_h -> MagicNumber == 3028){
-        int b_index = _h -> binary_Index -1;
-        int newsize = pow(2,b_index)* blocksize;
-        HDR * _hR = (Addr)((char*)_h + newsize);     // error need to change _hR's address 
-        // _remove _h from freelist
-        _remove(_h);
-        // set variables for _h and _hR
-    _hR -> inheritance = _h -> inheritance;
-    _h -> inheritance = _h -> side;
-    
-        _h -> MagicNumber = 3028;      
-        _h ->  is_free = 0;            
-        _h -> binary_Index = b_index;        
-        _h -> side = 0;                
-        _h -> size = newsize;                 
-        _h -> next = _hR;    
-        
-        _hR -> MagicNumber = 3028;      
-        _hR ->  is_free = 0;            
-        _hR -> binary_Index = b_index;        
-        _hR -> side = 1;                
-        _hR -> size = newsize;                 
-        _hR -> prev = _h;
-        // assign headers to freeList
-        if(freeList[b_index] == NULL){  
-          freeList[b_index] = _h;
-          _h -> prev = NULL;
-          _hR -> next = NULL;
-        }
-        else{
-          _hR -> next = freeList[b_index];
-          freeList[b_index] -> prev = _hR;
-          _h -> prev = NULL;
-          freeList[b_index] = _h;
-        }
-        printf("%s %u %d\n"," location of _h with size of:", _h, newsize);
-        printf("%s %u %d\n"," location of _hR with size of:", _hR , newsize);
-    }
+		if(_h -> binary_Index > 0){
+			int b_index = _h -> binary_Index -1;
+			int newsize = pow(2,b_index)* blocksize;
+			HDR * _hR = (Addr)((char*)_h + newsize);     // error need to change _hR's address 
+			// _remove _h from freelist
+			_remove(_h);
+			// set variables for _h and _hR
+		_hR -> inheritance = _h -> inheritance;
+		_h -> inheritance = _h -> side;
+		
+			_h -> MagicNumber = 3028;      
+			_h ->  is_free = 0;            
+			_h -> binary_Index = b_index;        
+			_h -> side = 0;                
+			_h -> size = newsize;                 
+			_h -> next = _hR;    
+			
+			_hR -> MagicNumber = 3028;      
+			_hR ->  is_free = 0;            
+			_hR -> binary_Index = b_index;        
+			_hR -> side = 1;                
+			_hR -> size = newsize;                 
+			_hR -> prev = _h;
+			// assign headers to freeList
+			if(freeList[b_index] == NULL){  
+			  freeList[b_index] = _h;
+			  _h -> prev = NULL;
+			  _hR -> next = NULL;
+			}
+			else{
+			  _hR -> next = freeList[b_index];
+			  freeList[b_index] -> prev = _hR;
+			  _h -> prev = NULL;
+			  freeList[b_index] = _h;
+			}
+			printf("%s %u %d\n"," location of _h with size of:", _h, newsize);
+			printf("%s %u %d\n"," location of _hR with size of:", _hR , newsize);	
+		}
+		else
+			printf("%s\n", "error: cannot split last block ");		
+	}		
     else
       printf("%s\n", "Error no magic number found, ");
     
@@ -143,78 +147,84 @@ void SplitBlocks ( HDR * _h){
 //*******************************************************
 void MergeBlocks ( HDR * _h){
   if(_h->MagicNumber == 3028){
-  int newindex = _h -> binary_Index +1;
-  int newsize = pow(2,newindex) * blocksize;
-  HDR * _h_ = NULL;
+	  if(_h -> binary_Index < index){
+	  int newindex = _h -> binary_Index +1;
+	  int newsize = pow(2,newindex) * blocksize;
+	  HDR * _h_ = NULL;
 
-      if (_h -> side == 0){                      // if right block side check if right block is free
-         HDR * temp = (Addr)((char*)_h + _h->size);
-         _h_ = temp;}
-      else{                                      // vice-versa
-         HDR * temp = (Addr)((char*)_h - _h->size);
-         _h_ = temp;}
+		  if (_h -> side == 0){                      // if right block side check if right block is free
+			 HDR * temp = (Addr)((char*)_h + _h->size);
+			 _h_ = temp;}
+		  else{                                      // vice-versa
+			 HDR * temp = (Addr)((char*)_h - _h->size);
+			 _h_ = temp;}
 
-      if(_h -> is_free == 0){                   
-        if(_h_ -> MagicNumber == 3028){
-            if(_h_ -> is_free == 0){             // _remove 2 blocks from free list
-              if(_h_ -> size == _h -> size){
-                 _remove(_h);
-                 _remove(_h_);
-                 if (_h -> side == 0){            // assign new values to the block on left (must check which block is left )
-                    _h -> MagicNumber = 3028;      
-                    _h ->  is_free = 0;            
-                    _h -> binary_Index = newindex;        
-                    _h -> side = _h -> inheritance;
-                    _h -> inheritance = _h_ -> inheritance;
-                    _h -> size = newsize; 
+		  if(_h -> is_free == 0){                   
+			if(_h_ -> MagicNumber == 3028){
+				if(_h_ -> is_free == 0){             // _remove 2 blocks from free list
+				  if(_h_ -> size == _h -> size){
+					 _remove(_h);
+					 _remove(_h_);
+					 if (_h -> side == 0){            // assign new values to the block on left (must check which block is left )
+						_h -> MagicNumber = 3028;      
+						_h ->  is_free = 0;            
+						_h -> binary_Index = newindex;        
+						_h -> side = _h -> inheritance;
+						_h -> inheritance = _h_ -> inheritance;
+						_h -> size = newsize; 
 
-                    if(freeList[newindex] == NULL){   // add the updated header into freeList
-                      freeList[newindex] = _h;
-                      _h -> prev = NULL;
-                      _h -> next = NULL;
-                    }
-                    else{
-                      _h -> next = freeList[newindex];
-                      freeList[newindex] -> prev = _h;
-                      _h -> prev = NULL;
-                      freeList[newindex] = _h;
-                    }                
-                     
-                 }
-                 else{
-                    _h_ -> MagicNumber = 3028;      
-                    _h_ ->  is_free = 0;            
-                    _h_ -> binary_Index = newindex;        
-                    _h_ -> side = _h_ -> inheritance;
-                    _h_ -> inheritance = _h -> inheritance;               
-                    _h_ -> size = newsize; 
+						if(freeList[newindex] == NULL){   // add the updated header into freeList
+						  freeList[newindex] = _h;
+						  _h -> prev = NULL;
+						  _h -> next = NULL;
+						}
+						else{
+						  _h -> next = freeList[newindex];
+						  freeList[newindex] -> prev = _h;
+						  _h -> prev = NULL;
+						  freeList[newindex] = _h;
+						}                
+						 MergeBlocks(_h);
+					 }
+					 else{
+						_h_ -> MagicNumber = 3028;      
+						_h_ ->  is_free = 0;            
+						_h_ -> binary_Index = newindex;        
+						_h_ -> side = _h_ -> inheritance;
+						_h_ -> inheritance = _h -> inheritance;               
+						_h_ -> size = newsize; 
 
-                    if(freeList[newindex] == NULL){     // add the updated header into freeList
-                      freeList[newindex] = _h_;
-                      _h_ -> prev = NULL;
-                      _h_ -> next = NULL;
-                    }
-                    else{
-                      _h_ -> next = freeList[newindex];
-                      freeList[newindex] -> prev = _h_;
-                      _h_ -> prev = NULL;
-                      freeList[newindex] = _h_;
-                    }
-                }
-              }
-              else
-                printf("%s\n","Not the same size blocks" );
-            }
-            else
-              printf("%s\n","Memory Location for second block is not free" );
+						if(freeList[newindex] == NULL){     // add the updated header into freeList
+						  freeList[newindex] = _h_;
+						  _h_ -> prev = NULL;
+						  _h_ -> next = NULL;
+						}
+						else{
+						  _h_ -> next = freeList[newindex];
+						  freeList[newindex] -> prev = _h_;
+						  _h_ -> prev = NULL;
+						  freeList[newindex] = _h_;
+						}
+						MergeBlocks(_h_);
+					}
+					
+				  }
+				  else
+					printf("%s\n","Not the same size blocks" );
+				}
+				else
+				  printf("%s\n","Memory Location for second block is not free" );
 
-        }
-        else
-          printf("%s\n","Memory Location for second block is not valid" );
-      }    
-      else
-        printf("%s\n","Memory Location for first block is not free" );
-      }
+			}
+			else
+			  printf("%s\n","Memory Location for second block is not valid" );
+		  }    
+		  else
+			printf("%s\n","Memory Location for first block is not free" );
+		}
+		  else
+			printf("%s\n","Cannot Merge Top Block");
+	}
   else
     printf("%s\n","Memory Location for initial block is not valid" );
 }
@@ -260,7 +270,7 @@ extern unsigned int init_allocator(unsigned int _basic_block_size, unsigned int 
   freeList[index] = a;  
   
   totalLength = BlockNum * blocksize;
-  totalMemory = totalMemory + sizeof(HDR);
+  totalMemory = totalMemory;
   printf("%s %d\n", "Index: ", index );
   return totalLength;
 
@@ -344,7 +354,7 @@ extern Addr my_malloc(unsigned int _length) {
      _length += 40;
     int valid_length = 0;
     int valid_index = 0;
-
+	HDR * return_hdr;
     if (_length < 40){
       printf("%s\n","Error: requested memory - too small");
     }
@@ -373,16 +383,24 @@ extern Addr my_malloc(unsigned int _length) {
         SplitBlocks(freeList[_index]);
         _index--;
       }
-      print_freeList();
+		print_freeList();
+		return_hdr = (HDR*)freeList[valid_index];
+		_remove(freeList[valid_index]);
+		print_freeList();
+//      printf("%s %u\n","Location of beginning totalMemory: ", totalMemory);
+//     printf("%s %u\n","Location of end of totalMemory: ", totalMemory + *****);
+      printf("%s %u\n","Normal Address(i.e. header location): ", (Addr)((char*)return_hdr ));
+      printf("%s %u\n","Return Address: ", (Addr)((char*)return_hdr+sizeof(HDR) ));
+	  if(return_hdr -> MagicNumber == 3028){
+			return_hdr -> is_free = 1;
+			printf("%s \n","My Malloc.... header in correct location ");
+		}
+	  else
+			printf("%s \n","My Malloc....WRONG LOCATION ");
 
-      printf("%s %u\n","Location of beginning totalMemory: ", totalMemory);
-      printf("%s %u\n","Location of end of totalMemory: ", totalMemory + sizeof(HDR));
-      printf("%s %u\n","Normal Address: ", (Addr)((char*)freeList[valid_index] ));
-      printf("%s %u\n","Reutrn Address: ", (Addr)((char*)freeList[valid_index]+sizeof(HDR) ));
-      return (Addr)((char*)(freeList[valid_index] + sizeof(HDR)));
+      return (Addr)((char*)return_hdr + sizeof(HDR));
     }
   }
-// HDR * _hR = (Addr)((char*)_h + newsize); 
 
 
 }
@@ -391,11 +409,34 @@ extern int my_free(Addr _a) {
 
 //  if (_a < totalMemory || _a > (totalMemory + (blocksize * BlockNum) ))
  //     printf("%s\n","Address out of bounds");
-//  else{
-    _a = (HDR *)(_a - sizeof(HDR));
-    printf("%s %u\n","Header for a: ", _a );
-   // printf("%d\n", temp -> MagicNumber );
-//  }
+	if(_a == NULL){
+		printf("%s \n","error: address is NULL ");
+		return 1;
+	}
+	else{
+
+   // _a =  (Addr)((char*)(_a - sizeof(HDR)));
+		HDR* temp = (HDR*)((char*)_a - sizeof(HDR));
+		printf("\n\n%s %d\n\n","Calling my_free on....", temp->size);
+		//printf("%s %u\n","Header for temp: ", temp );
+		//printf("%d\n", temp -> MagicNumber );
+		int newindex = temp->binary_Index;
+		temp -> is_free = 0;
+		
+		if(freeList[newindex] == NULL){     // add the updated header into freeList
+            freeList[newindex] = temp;
+             temp -> prev = NULL;
+             temp -> next = NULL;
+         }
+         else{
+            temp -> next = freeList[newindex];
+            freeList[newindex] -> prev = temp;
+            temp -> prev = NULL;
+            freeList[newindex] = temp;
+         }
+		 MergeBlocks(temp);
+  }
+  print_freeList();
   return 0;
 }
 
