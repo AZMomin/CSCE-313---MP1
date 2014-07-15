574b5ab7b3afccbfdc12fde1f6f2703028d1ac19
AZMomin <ali.shan1014@gmail.com>
2014-07-15 03:04:16 -0500

    Update memtest.c

--- memtest.c
+++ memtest.c
@@ -2,11 +2,12 @@
 #include "my_allocator.h"
 
 int main(int argc, char ** argv) {
-	int a = init_allocator(4, 10000);
+	int a = init_allocator(4, 100000);
 	printf("%s %d\n", "Total memory allocated" ,a );
 	printf("%s %d\n", "Total blocks" ,a/4 );
-	print_freeList();
-	check_list();
-	print_freeList();
-	return 0;
+	//print_freeList();
+	//check_list();
+	//print_freeList();
+	void * b = my_malloc(16000);
+	int x = my_free(b);
 }
