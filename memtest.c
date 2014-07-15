3a5b23ba841fe09190f4f06831fafdd30c08b5ff
AZMomin <ali.shan1014@gmail.com>
2014-07-15 17:02:43 -0500

    Update memtest.c

--- memtest.c
+++ memtest.c
@@ -9,5 +9,13 @@ int main(int argc, char ** argv) {
 	//check_list();
 	//print_freeList();
 	void * b = my_malloc(16000);
+	void * d = my_malloc(16000);
+	void * e = my_malloc(16000);
+	void * f = my_malloc(16000);
+	void * c = my_malloc(81000);
 	int x = my_free(b);
+	int y = my_free(d);
+	int r = my_free(e);
+	int t = my_free(f);
+	int p = my_free(c);
 }
